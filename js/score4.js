a971010aaa2a0d9c9e8fbd544698db61586137d0
air-h-128k-il <air.h.128k.il@gmail.com>
2017-09-01 16:28:32 +0900

    add js/---4.js files

---
+++ js/score4.js
@@ -0,0 +1,356 @@
+﻿//
+// 棋譜共通処理
+//
+
+var ie6 = (navigator.userAgent.indexOf('MSIE 6.0') >= 0);
+var ie7 = (navigator.userAgent.indexOf('MSIE 7.0') >= 0);
+var ie8 = (navigator.userAgent.indexOf('MSIE 8.0') >= 0);
+
+var nsArray = new Array();
+var nsIndex = -1;// -1 っすか。
+var nsInter;
+var nsDraw;
+var nsSoundEnabled = false;
+var nsSoundRequest = false;
+var nsSoundName = null;
+var nsAudio = null;
+var nsAudioIndex = 0;
+var nsType;
+var nsSpeedCounter = 0;
+var nsSpeedCoeff   = 2;
+var nsInitialField = "1b191716101617191b0014000000000012001d1d1d1d1d1d1d1d1d"
+					 + "0000000000000000000000000000000000000000000000000000000"
+					 + "e0e0e0e0e0e0e0e0e0003000000000005000c0a08070207080a0c00";
+
+// 棋譜読み込み
+
+
+
+function Start(name, type){
+	// ブラウザチェック
+	//if ( ie6 ){
+	//	alert('Internet Explorer 6 以下には対応していません');
+	//	return;
+	//}
+
+	// 棋譜
+//	var d = name.match(/^[0-9]{8}$/);
+//	var t = type.match(/^[is]$/);
+//	var q = './score.cgi';
+//	if ( (d != null) && (t != null) ){
+//		q += '?d=' + d + '&t=' + t;
+//	}
+//	var q = '..data/';
+	var q = name;
+//	alert(q);
+	var s = httpRead(q);
+//	alert(s);
+	nsArray = nsMakeScore(s);
+	nsType  = 's';	
+
+	if ( nsArray.Title != null ){
+		nsSetInfo('sctitle', nsArray.Title);
+		//nsSetInfo('scstage', nsArray.Stage);
+		var list = nsArray.Stage.split(' ');
+		nsSetInfo('scstage', list[0]);
+		if ( list.length >= 2 ){
+			nsSetInfo('scstage2', list[1]);
+		}
+		if ( nsArray.Notify != null ){
+			nsSetInfo('scnotify', nsArray.Notify);
+		}
+		if ( nsArray.Notify != null ){
+			nsSetInfo('scnotify2', nsArray.Notify2);
+		}
+	}
+	else{
+		nsSetInfo('sctitle', 'の棋譜はありません');
+		nsSetInfo('scstage', '');
+	}
+	nsSetInfo('scyear',  '20' + nsArray.OnAir);
+	//nsSetInfo('scmonth', nsArray.OnAir.substr(4, 2));
+	//nsSetInfo('scday',   nsArray.OnAir.substr(6, 2));
+	var p1 = nsSplitName(nsArray.Player1);
+	var p2 = nsSplitName(nsArray.Player2);
+	nsSetInfo('p1name',  '▲' + p1.familyname + p1.firstname + ' ' + p1.title);
+	//nsSetInfo('p1title', p1.title);
+	//nsSetInfo('p1title2', p1.title2);
+	nsSetInfo('p2name', '△'+ p2.familyname + p2.firstname + ' ' + p2.title);
+	//nsSetInfo('p2title', p2.title);
+	//nsSetInfo('p2title2', p2.title2);
+	
+}
+
+
+// 打音セット
+function nsSetSound(name){
+	if ( Audio ){
+		nsAudio = new Array();
+		for ( var i = 0; i < 8; i++ ){
+			var audio = new Audio(name);
+			audio.preload  = 'auto';
+			audio.autoplay = false;
+			audio.load();
+			nsAudio.push(audio);
+		}
+	}
+	else{
+		nsSoundName = name;
+	}
+}
+
+function nsSoundToggle(elem){
+	nsSoundEnabled = elem.checked;
+}
+
+// 情報をセット
+function nsSetInfo(id, value){
+	var e = document.getElementById(id);
+	if ( e != null ){
+		e.innerText = value;
+		e.textContent = value;
+	}
+}
+
+// スピードセット
+function nsSetSpeed(speed){
+	nsSpeedCoeff = speed;
+}
+
+// 棋譜を配列に整形
+function nsMakeScore(str){
+	var res = new Array();
+	res["buf"] = new Array();
+
+	var list = str.split('\n');
+	var idx = -1;
+	for ( var i = 1; i < list.length; i++ ){//1行目の NPS100 は捨てる
+		var s = list[i];
+		if ( s.length > 0 ){
+			var pair = s.split('=');
+			if ( pair.length == 2 ){
+				var n = pair[0];//n name v value でしょうか
+				var v = pair[1].replace(";", "");
+				v = v.replace("\r", "");
+				if ( n.match(/^[phc]/) ){//局面情報やコメントはこちらでしょうか
+					if ( n == 'p' ){//局面の最初の行は p 。
+						++idx;//最初の行で -1+1=0 から格納
+						res.buf[idx] = new Array();
+						res.buf[idx][n] = v;
+					}
+					else{
+						res.buf[idx][n] = v;
+					}
+				}
+				else{
+					res[n] = v;//OnAir,Title などはこっちか
+				}
+			}
+		}
+	}
+	return res;
+}
+
+// index 番目の手番へ
+function nsSetIndex(draw, index){
+	nsDraw = draw;
+	if (index != nsIndex){
+		nsIndex = index;
+
+		// 描画
+		if ( nsIndex >= 0 ){
+			if ( nsType == 'i' ){
+				// 囲碁の場合はそのまま
+				draw(nsArray.buf[nsIndex]);
+				nsSound();
+			}
+			else{
+				// 将棋の場合は遅延描画
+				draw((nsIndex > 0)? nsArray.buf[nsIndex - 1]: null);
+				nsAnalyzeMarkPos(nsArray.buf[nsIndex].p,
+								 (nsIndex > 1)? nsArray.buf[nsIndex- 1].p: nsInitialField);
+				setTimeout("nsDelayDraw()", 500);
+			}
+		}
+		else{
+			draw(null);
+			cmMoveMark(0, 0);
+		}
+
+		// ステップ数表示
+		var s = '';
+		if ( (nsType == 'i') && (nsIndex >= nsArray.buf.length - 1) ){
+			s = nsIgoResult();
+		}
+		else if ( nsIndex >= 0 ){
+			s = (nsIndex + 1) + '手目';
+		}
+		nsSetInfo('move', s);//修正
+	}
+}
+
+
+
+// 結果を文字列に変換
+function nsShogiResult(){
+	var res = (nsIndex + 1) + '手　';
+	switch ( nsArray.Result ){
+	case '1':
+		var p1 = nsSplitName(nsArray.Player1);
+		res += '先手　' + p1.familyname + p1.firstname + p1.title +  'の勝ち';
+		break;
+	case '2':
+		var p2 = nsSplitName(nsArray.Player2);
+		res += '後手　' + p2.familyname + p2.firstname + p2.title +  'の勝ち';
+		break;
+	default:
+		res = '持将棋';
+	}
+
+	return res;
+}
+
+// 姓名・タイトルを取得
+function nsSplitName(text){
+	var res = new Array();
+	var p = text.split(' ');
+
+	res.familyname = p[0];
+	res.firstname  = '';
+	res.title      = '';
+	res.title2     = '';
+	switch ( p.length ){
+	case 2:
+		res.title = p[1];
+		break;
+	case 3:
+		res.firstname = p[1];
+		res.title = p[2];
+		break;
+	case 4:
+		res.firstname = p[1];
+		res.title = p[2];
+		res.title2 = p[3];
+		break;
+	}
+
+	return res;
+}
+
+// 再生
+function nsPlay(draw){
+	var src = '';
+	if ( nsInter == null ){
+		nsDraw = draw;
+		// 最初は即座に描画
+		nsInterval();
+		// タイマーセット
+		nsInter = setInterval("nsInterval()", 1200);
+		src = "https://air-h-128k-il.github.io/img/move_item05a.png";
+	}
+	else{
+		clearInterval(nsInter);
+		nsInter = null;
+		src = "https://air-h-128k-il.github.io/img/move_item05.png";
+	}
+	var e = document.getElementById('playbutton');
+	e.src = src;
+}
+
+// 自動再生処理
+function nsInterval(){
+	if ( ++nsSpeedCounter >= nsSpeedCoeff ){
+		nsStep(nsDraw, 1);
+		if ( nsIndex >= nsArray.buf.length - 1 ){
+			clearInterval(nsInter);
+			var e = document.getElementById('playbutton');
+			e.src = "https://air-h-128k-il.github.io/img/move_item05.png";
+		}
+		nsSpeedCounter = 0;
+	}
+}
+
+// 一時停止
+function nsPause(){
+	clearInterval(nsInter);
+}
+
+// n手進める
+function nsStep(draw, n){
+	if ( nsSoundEnabled ){
+		nsSoundRequest = true;
+	}
+	nsSetIndex(draw, Math.min(nsIndex + n, nsArray.buf.length - 1));
+}
+
+// n手戻す
+function nsBack(draw, n){
+	nsSetIndex(draw, Math.max(nsIndex - n, -1));
+}
+
+// 先頭へ
+function nsHead(draw){
+	nsSetIndex(draw, -1);
+}
+
+// 末尾へ
+function nsTail(draw){
+	nsSetIndex(draw, nsArray.buf.length - 1);
+}
+
+// マーカー位置計算
+function nsAnalyzeMarkPos(curr, prev){
+	var x0 = 0;
+	var y0 = 0;
+	if ( (curr != null) && (prev != null) ){
+		var x1 = parseInt(curr.substr(81 * 2,     1));
+		var y1 = parseInt(curr.substr(81 * 2 + 1, 1));
+		var pos1 = (x1 - 1) + (y1 - 1) * 9;
+		for ( var i = 0; i < 81; i++ ){
+			if ( (curr.substr(i * 2, 2) != prev.substr(i * 2, 2)) && (i != pos1) ){
+				x0 = (i % 9) + 1;
+				y0 = Math.floor(i / 9) + 1;
+				break;
+			}
+		}
+	}
+	cmMoveMark(x0, y0);
+}
+
+// マーカー移動描画
+function nsDelayDraw(){
+	// マーカー移動
+	if ( nsIndex >= 0 ){
+		nsDraw(nsArray.buf[nsIndex]);
+		nsSound();
+	}
+
+	// 将棋の最終結果遅延表示
+	if ( (nsType != 'i') && (nsIndex >= nsArray.buf.length - 1) ){
+		// 最終手の移動完了と同時に表示
+		nsSetInfo('move', nsShogiResult());
+		// 最終手の移動完了よりさらに遅らせる場合
+		//var s = "nsSetInfo('bante', '" + nsShogiResult() + "')";
+		//setTimeout(s, 500);
+	}
+}
+
+// 音
+function nsSound(){
+	if ( nsSoundRequest ){
+		if ( nsAudio != null ){
+			nsAudio[nsAudioIndex].play();
+			nsAudioIndex = (nsAudioIndex + 1) % nsAudio.length;
+		}
+		else if ( nsSoundName != null ){
+			var e = document.getElementById('sound2');
+			if ( e != null ){
+				e.innerHTML = '<embed id="sound2embed" src="'
+							  + nsSoundName
+							  + '" autostart="true" hidden="true">';
+			}
+		}
+		nsSoundRequest = false;
+	}
+}
+
---
+++ js/shogi4.js
@@ -0,0 +1,287 @@
+//
+// 将棋棋譜描画処理
+//
+
+
+var baseX = 0;
+var baseY = 0;
+var pieceW = 36.1;
+var pieceH = 39.3;
+var handH1 = 35.3;
+var handH2 = 35.3;
+var handCount = 38;
+var markX = -1;
+var markY = -1;
+var markW = 36.0;
+var markH = 39.0;
+var sgPicNames = new Array(
+		'emp',
+		'ou0', 'gy0', 'hi0', 'ry0', 'ka0', 'um0',
+		'ki0', 'gi0', 'gn0', 'ke0', 'kn0', 'ky0', 'kyn0', 'fu0', 'to0',
+		'ou1', 'gy1', 'hi1', 'ry1', 'ka1', 'um1',
+		'ki1', 'gi1', 'gn1', 'ke1', 'kn1', 'ky1', 'kyn1', 'fu1', 'to1',
+		'mark'
+);
+var sgPic = new Array();
+
+// 初期化
+function sgInit(){
+	var parent = document.getElementById('board');
+	sgResetCoord(parent);
+
+	for ( var i = 0; i < sgPicNames.length; i++ ){
+		var name = sgPicNames[i];
+		sgPic[name] = new Image();
+//		if ( ie6 ){
+//			sgPic[name].src = 'img/pieces2/' + name + '.gif';
+//		}
+//		else{
+			sgPic[name].src = 'https://air-h-128k-il.github.io/img/pieces/' + name + '.png';
+//		}
+	}
+
+	// マーカー追加
+	var e = document.createElement('img');
+	e.id  = 'mark';
+	e.src = sgPic.mark.src;
+	e.alt = '';
+	e.width  = 35;
+	e.height = 38;
+	e.style.position = 'absolute';
+	sgShowMark(e);
+	parent.appendChild(e);
+
+	// 盤面の表示位置
+	for ( var i = 0; i < 81; i++ ){
+		var e = document.createElement('img');
+		e.id  = 'g' + i;
+		e.src = sgPic.emp.src;
+		e.alt = '';
+		e.width  = 36;
+		e.height = 43;
+		e.style.position = 'absolute';
+	  e.style.left = baseX + ((8 - i % 9) * pieceW) + 'px';
+	  e.style.top  = baseY + (Math.floor(i / 9) * pieceH) + 'px';
+		parent.appendChild(e);
+	}
+	// 持駒の表示位置
+	for ( var i = handCount - 1; i >= 0; i-- ){
+		var e = document.createElement('img');
+		e.id  = 'ga' + i;
+		e.src = sgPic.emp.src;
+		e.alt = '';
+		e.width  = 36;
+		e.height = 43;
+		e.style.position = 'absolute';
+		e.style.left = baseX + 345 + ((i % 3) * pieceW) + 'px';
+		e.style.top  = baseY + 255 + (Math.floor(i / 3) * handH1) + 'px';   
+		parent.appendChild(e);
+	}
+	// 持駒の表示位置
+	for ( var i = handCount - 1; i >= 0; i-- ){
+		var e = document.createElement('img');
+		e.id  = 'gb' + i;
+		e.src = sgPic.emp.src;
+		e.alt = '';
+		e.width  = 36;
+		e.height = 43;
+		e.style.position = 'absolute';
+    	e.style.left = baseX - 60 - ((i % 3) * pieceW) + 'px';
+		e.style.top  = baseY + 60 - (Math.floor(i / 3) * handH2) + 'px';
+		parent.appendChild(e);
+	}
+
+	sgClear();
+}
+
+// リサイズ時処理
+function sgResize(){
+	var parent = document.getElementById('board');
+	sgResetCoord(parent);
+
+	sgShowMark(null);
+
+	// 盤面の表示位置
+	for ( var i = 0; i < 81; i++ ){
+		var e = document.getElementById('g' + i);
+		e.style.left = baseX + ((8 - i % 9) * pieceW) + 'px';
+		e.style.top  = baseY + (Math.floor(i / 9) * pieceH) + 'px';
+	}
+	// 持駒の表示位置
+	for ( var i = 0; i < handCount; i++ ){
+		var e = document.getElementById('ga' + i);
+		e.style.left = baseX + 345 + ((i % 3) * pieceW) + 'px';
+		e.style.top  = baseY + 255 + (Math.floor(i / 3) * handH1) + 'px';
+	}
+	// 持駒の表示位置
+	for ( var i = 0; i < handCount; i++ ){
+		var e = document.getElementById('gb' + i);
+		e.style.left = baseX - 60 - ((i % 3) * pieceW) + 'px';
+		e.style.top  = baseY + 60 - (Math.floor(i / 3) * handH2) + 'px';
+	}
+}
+
+// 座標再設定
+function sgResetCoord(parent){
+ baseX = 138;//修正
+ baseY = 25.5;//修正
+}
+
+// クリア
+function sgClear(){
+	for ( var i = 0; i < 81; i++ ){
+		var n = 'emp';
+		switch ( i ){
+			case 0: n = 'ky1'; break;
+			case 1: n = 'ke1'; break;
+			case 2: n = 'gi1'; break;
+			case 3: n = 'ki1'; break;
+			case 4: n = 'ou1'; break;
+			case 5: n = 'ki1'; break;
+			case 6: n = 'gi1'; break;
+			case 7: n = 'ke1'; break;
+			case 8: n = 'ky1'; break;
+			case 10: n = 'ka1'; break;
+			case 16: n = 'hi1'; break;
+			case 18:
+			case 19:
+			case 20:
+			case 21:
+			case 22:
+			case 23:
+			case 24:
+			case 25:
+			case 26: n = 'fu1'; break;
+			case 54:
+			case 55:
+			case 56:
+			case 57:
+			case 58:
+			case 59:
+			case 60:
+			case 61:
+			case 62: n = 'fu0'; break;
+			case 64: n = 'hi0'; break;
+			case 70: n = 'ka0'; break;
+			case 72: n = 'ky0'; break;
+			case 73: n = 'ke0'; break;
+			case 74: n = 'gi0'; break;
+			case 75: n = 'ki0'; break;
+			case 76: n = 'gy0'; break;
+			case 77: n = 'ki0'; break;
+			case 78: n = 'gi0'; break;
+			case 79: n = 'ke0'; break;
+			case 80: n = 'ky0'; break;
+		}
+		var e = document.getElementById('g' + i);//g0 とかが位置かな
+		e.src = sgPic[n].src;
+	}
+	for ( var i = 0; i < handCount; i++ ){
+		var e;
+		e = document.getElementById('ga' + i);
+		e.src = sgPic.emp.src;
+		e = document.getElementById('gb' + i);
+		e.src = sgPic.emp.src;
+	}
+}
+
+// 描画
+function sgDraw(item){
+	if ( item == null ){
+		sgClear();
+	}
+	else{
+		// 盤面表示
+		var s = item.p;//
+		for ( var i = 0; i < 81; i++ ){
+			var e = document.getElementById('g' + i);
+			var t = parseInt(s.substr(i * 2, 2), 16);//substr(a,b) aの位置からb文字切り出す。parseInt(a,16) a を１６進数で解釈し、１０進数で返す。
+			//alert(t);
+			if ( t <= 31 ){
+				e.src = sgPic[sgPicNames[t]].src;
+			}
+		}
+
+		// マーク
+		markX = parseInt(item.p.substr(81 * 2,     1));
+		markY = parseInt(item.p.substr(81 * 2 + 1, 1));
+		sgShowMark(null);
+
+		// 持駒表示
+		sgDrawHand(item.h1, 'ga');
+		sgDrawHand(item.h2, 'gb');
+		
+		// コメント表示　item.c が null でなければ内容を表示
+		var u = document.getElementById('comment');
+		//alert(item.c);
+		if (item.c){
+			//alert(item.c);
+			u.innerText = item.c;
+			u.style.display = '';
+		}else{
+			u.innerText = "";
+			//u.style.display = 'none';
+		}
+		
+	}
+}
+
+// 持駒描画
+function sgDrawHand(hand, id){
+	var n = hand.length / 2;
+
+	// 持駒の数によって表示間隔を調整
+	var h = (n <= 9)? 35.3: (90 / Math.floor((n + 2) / 3));
+	if ( id == 'ga' ){
+		if ( handH1 != h ){
+			handH1 = h;
+			sgResize();
+		}
+	}
+	else{
+		if ( handH2 != h ){
+			handH2 = h;
+			sgResize();
+		}
+	}
+
+	// 持駒を配列に入れて強さの順にソート
+	var pa = new Array();
+	for ( var i = 0; i < n; i++ ){
+		pa[i] = parseInt(hand.substr(i * 2, 2), 16);
+	}
+	pa.sort(function(a, b){ return a - b; });
+
+	// 持駒表示
+	for ( var i = 0; i < handCount; i++ ){
+		var e = document.getElementById(id + i);
+		if ( i < n ){
+			var t = pa[i];
+			if ( t <= 31 ){
+				e.src = sgPic[sgPicNames[t]].src;
+			}
+		}
+		else{
+			e.src = sgPic.emp.src;
+		}
+	}
+}
+
+function cmMoveMark(x, y){
+	markX = x;
+	markY = y;
+	sgShowMark(null);
+}
+
+function sgShowMark(elem){
+	var e = (elem != null)? elem: document.getElementById('mark');
+	if ( (markX > 0) && (markY > 0) ){
+		e.style.left = (baseX + (9 - markX) * markW + 1).toString() + 'px';
+		e.style.top  = (baseY + (markY - 1) * markH + 4).toString() + 'px';
+		e.style.display = '';
+	}
+	else{
+		e.style.display = 'none';
+	}
+}
+
